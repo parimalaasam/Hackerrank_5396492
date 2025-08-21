c391aa60f8e3b9aca1268945d54522bd0289930e
parimalaasam <parimalaasam6463@gmail.com>
2025-08-21 19:17:39 +0530

    fix week2

---
+++ Week 2/mock Test/palindrome.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+    // Write your code here
+    int a=0;
+    int b=s.length()-1;
+    while(a<b && s.charAt(a)==s.charAt(b)){
+        a++;
+        b--;
+    }
+    if(a>=b){
+        return -1;
+    }
+    if(ispal(s,a+1,b)){
+        return a;
+    }
+    if(ispal(s,a,b-1)){
+        return b;
+    }
+    return -1;
+
+    }
+    public static boolean ispal(String s, int a,int b){
+        while(a<b && s.charAt(a)==s.charAt(b)){
+            a++;
+            b--;
+        }
+        return a>=b;
+    }
+    
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
